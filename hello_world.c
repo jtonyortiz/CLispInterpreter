fb5f1847715e403bfbedab3bef3d8f6f85c1bab2
James Ortiz <jamesaortiz75@gmail.com>
2019-11-10 12:23:01 -0500

    Initial Program

---
+++ hello_world.c
@@ -0,0 +1,17 @@
+//========================================================
+//File: hello_world.c
+//Author: James Anthony Ortiz
+//Description: Introductory file to program.
+//Compile on Windows / Linux OS:
+//gcc -std=c99 -Wall -g -o hello_world hello_world.c
+//=========================================================
+
+//Used for Input/Output:
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	//Put String:
+	puts("Hello, world!");
+	return 0;
+}//end main
